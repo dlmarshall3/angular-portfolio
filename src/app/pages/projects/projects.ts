0d22aa42140ee15f5a2d4c8c0c47609a901f5b2d
david-carmigo <dlmarshall3@protonmail.com>
2023-03-26 00:17:54 -0500

    added portfolio site to projects

---
+++ src/app/pages/projects/projects.ts
@@ -0,0 +1,26 @@
+export const Projects =[
+    {
+        projectName: 'Portfolio Website',
+        projectDescription: `When I design an application, my goal is to create what is necessary, and nothing more. I'm a huge fan of minimalist design, so with this portfolio site, I wanted to emphasize a user-friendly experience rather than animations galore. Even the generic buttons are intentional. (I personally love the combination of a gray background, square corners and IBM font.)`,
+        projectRepo: 'https://github.com/dlmarshall3/angular-portfolio',
+        projectDemo: 'https://davidmarshall.dev/',
+        projectImage: 'assets/portfolio.png',
+        npmPackage: '',
+      },
+    {
+        projectName: 'Kaiju Calculator',
+        projectDescription: `Even though I try to build applications to improve my skills, I tend to design them for my kids, and this one was no different. This particular idea came to mind as I was thinking of how funny it\'d be for someone to be pulled over by the police for not knowing what a speed limit of "1770 Godzillas Per Hour" meant. People may debate whether the heights of the monsters are correct (depending on the movie), but the math checks out. I promise.`,
+        projectRepo: 'https://github.com/dlmarshall3/kaiju-calculator',
+        projectDemo: 'https://kaiju-calculator.vercel.app/',
+        projectImage: 'assets/kaiju-calculator.png',
+        npmPackage: '',
+      },
+      {
+        projectName: 'Homestar Ipsum Generator',
+        projectDescription: 'What happens when you throw a bunch of Strong Bad emails into a blender? The Homestar Ipsum Generator, that\'s what. Two page application built with Angular. I also turned it into an installable npm package.',
+        projectRepo: 'https://github.com/dlmarshall3/homestar-ipsum-angular',
+        projectDemo: 'https://homestar-ipsum.vercel.app/',
+        projectImage: 'assets/homestar-ipsum.png',
+        npmPackage: 'https://www.npmjs.com/package/homestar-ipsum'
+      },
+]
