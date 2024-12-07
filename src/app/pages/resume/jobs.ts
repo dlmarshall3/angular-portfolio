37435e776235d495b1543ca7a09c79211cbbb1cd
David Marshall <dlmarshall3@protonmail.com>
2024-12-07 13:02:24 -0600

    resume changes

--- src/app/pages/resume/jobs.ts
+++ src/app/pages/resume/jobs.ts
@@ -40,9 +40,8 @@ export const Jobs = [
     location: 'Tupelo, MS (Remote)',
     time: ' Jan. 2022 - June 2022',
     experience: [
-      'Used Node.js and PostgreSQL to serve up dynamically-generated JavaScript scripts for dealerships to generate leads on their website without having to store a unique file in our server for each business.',
-      'Created new Vue components, routes, controllers, and repositories to set up a more efficient login process for our sellers and buyers that can be easily implemented throughout our application.',
-      'Continually utilized my extensive experience in the automotive industry to offer suggestions and advice to help with product improvement.',
+      'Implemented dynamic JavaScript generation with Node.js and PostgreSQL, enabling dealerships to have unique lead generation scripts without storing individual scripts in our database.',
+      'Leveraged automotive industry expertise to provide actionable insights for product enhancements, including debugging when various APIs were returning incorrect vehicle data.',
     ],
   },
   {
@@ -51,11 +50,8 @@ export const Jobs = [
     location: 'Blue Springs, MO',
     time: 'Jul. 2020 - Jan. 2022',
     experience: [
-      'Earned the dealership $300,000 in gross profit (Jan-Oct 2021), averaging $2800 per sale ($500 over the average in 2017).',
-      'Oversaw inventory life cycle, including stocking, scheduling inspections to ensure compliance/readiness to meet demands, and ordering parts.',
-      'Developed and implemented processes in Asana to reduce time between arrival, service, and sale, thereby minimizing time on the lot and maximizing dealership profit.',
-      'Acted as the first point of contact with online clients, ensuring they received all of the requested and relevant information.',
-      'Assisted clients with the purchasing process from beginning to end, including securing financing and selling warranties.',
+      'Earned the dealership over $400,000 in gross profit (fiscal year 2021), averaging $2800 per sale ($500 over the average in 2017).',
+      'Developed and implemented processes in Asana to reduce time between arrival, service, and sale, thereby minimizing time on the lot by an average of 7 days and maximizing dealership profit.',
     ],
   },
 ];
